1f62c15055fb958e4e4b823d4de7a947648527b9
Daniel Ang <daniel@sysanalyst.eu>
2022-07-08 21:33:32 +0200

    Contains with caseSensitive and wholeWord

--- src/Payloads/ValidateStringPayload.php
+++ src/Payloads/ValidateStringPayload.php
@@ -6,6 +6,10 @@ class ValidateStringPayload extends Payload
 {
     protected string $content;
 
+    protected bool $caseSensitive = false;
+
+    protected bool $wholeWord = false;
+
     public function __construct(
         public string $type
     ) {
@@ -16,18 +20,34 @@ class ValidateStringPayload extends Payload
         return 'validate';
     }
 
-    /** @return array<string> */
+    /** @return array<string|boolean> */
     public function content(): array
     {
         return [
-            'type'    => $this->type,
-            'content' => $this->content ?? '',
+            'type'              => $this->type,
+            'content'           => $this->content ?? '',
+            'is_case_sensitive' => $this->caseSensitive,
+            'is_whole_word'     => $this->wholeWord,
         ];
     }
 
     public function setContent(string $content): self
     {
         $this->content = $content;
+
+        return $this;
+    }
+
+    public function setCaseSensitive(bool $caseSensitive = true): self
+    {
+        $this->caseSensitive = $caseSensitive;
+
+        return $this;
+    }
+
+    public function SetWholeWord(bool $wholeWord = true): self
+    {
+        $this->wholeWord = $wholeWord;
 
         return $this;
     }
