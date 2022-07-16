25e3ac9a5c14ce148c618963a5e5071a23ec1276
Daniel Ang <daniel@sysanalyst.eu>
2022-07-16 17:44:04 +0200

    Include dsq and remove spaces

--- config/laradumps.php
+++ config/laradumps.php
@@ -224,19 +224,19 @@ return [
 
     'ci_check' => [
         'directories' => [
-            base_path('config'),
+            base_path('app'),
         ],
         'ignore_line_when_contains_text' => [
         ],
         'text_to_search' => [
-            ' ds(',
-            ' dsd(',
-            ' ds1(',
-            ' ds2(',
-            ' ds3(',
-            ' ds4(',
-            ' ds5(',
-            '@ds(',
+            'ds(',
+            'dsq(',
+            'dsd(',
+            'ds1(',
+            'ds2(',
+            'ds3(',
+            'ds4(',
+            'ds5(',
         ],
     ],
 
