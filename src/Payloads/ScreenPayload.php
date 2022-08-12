9c9e668774cd2e0e29180f1a7f058df240215796
sue <luanfreitasgoncalves@gmail.com>
2022-08-12 17:48:51 -0300

    remove svg from payload

--- src/Payloads/ScreenPayload.php
+++ src/Payloads/ScreenPayload.php
@@ -5,14 +5,10 @@ namespace LaraDumps\LaraDumps\Payloads;
 class ScreenPayload extends Payload
 {
     public function __construct(
-        public string $screenHtml,
+        public string $screenName,
         public bool $classAttr = false,
         public int $raiseIn = 0,
-        public ?string $screenName = '',
     ) {
-        if (empty($this->screenName)) {
-            $this->screenName = $this->screenHtml;
-        }
     }
 
     public function type(): string
@@ -25,11 +21,10 @@ class ScreenPayload extends Payload
     {
         /** @var array $config */
         $config    = config('laradumps.screen_btn_colors_map');
-        $classAttr = ($this->classAttr) ? $config[$this->screenHtml] : $config['default'];
+        $classAttr = ($this->classAttr) ? $config[$this->screenName] : $config['default'];
 
         return [
             'screenName' => $this->screenName,
-            'screenHtml' => $this->screenHtml,
             'classAttr'  => $classAttr,
             'raiseIn'    => $this->raiseIn,
         ];
