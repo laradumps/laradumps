10aab5d646b72f80a912627426c086230444918c
Dan Ang <daniel@sysanalyst.eu>
2022-10-11 09:54:52 +0200

    Improve switch component

--- resources/views/components/switch.blade.php
+++ resources/views/components/switch.blade.php
@@ -1,7 +1,24 @@
 <div>
+  <script>
+    window.onload=()=>{
+      // Send uncheck toggles as hidden inputs
+      const form = document.getElementById('form');
+      const toggles = [...document.querySelectorAll(".toggle-checkbox")];
+            
+      form.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        toggles.forEach((toggle) => {
+          if (toggle.checked === false) {
+            form.append(Object.assign(document.createElement('input'),{type: 'hidden', name: toggle.name, value: '0'}));
+          }
+        });
+        
+        form.submit();
+      });
+    };
+  </script>
   <style>
-    /* CHECKBOX TOGGLE SWITCH */
-    /* @apply rules for documentation, these do not work as inline style */
     .toggle-checkbox:checked {
       @apply: right-0 border-yellow-400;
       right: 0;
@@ -13,8 +30,7 @@
     }
     </style>
     <div class="relative inline-block w-10 mr-2 align-middle select-none transition duration-200 ease-in">
-      <input type="hidden" name="{{ $configKey['env_key'] }}" id="{{ $configKey['env_key'] }}" value="0">
         <input @checked($configKey['current_value']) value="1" type="checkbox" name="{{ $configKey['env_key'] }}" id="{{ $configKey['env_key'] }}" class="toggle-checkbox absolute block w-6 h-6 rounded-full bg-white border-4 appearance-none cursor-pointer"/>
-        <label for="toggle" class="toggle-label block overflow-hidden h-6 rounded-full bg-gray-300 cursor-pointer"></label>
+        <label for="{{ $configKey['env_key'] }}" class="toggle-label block overflow-hidden h-6 rounded-full bg-gray-300 cursor-pointer"></label>
     </div>
 </div>
