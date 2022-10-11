79ade27bfa46177aac2e8dc42e4acadd60005e7f
Dan Ang <daniel@sysanalyst.eu>
2022-10-11 09:54:46 +0200

    Divide page into tabs

--- resources/views/config.blade.php
+++ resources/views/config.blade.php
@@ -14,64 +14,58 @@
     </style>
     <script src="https://cdn.tailwindcss.com"></script>
   </head>
-  <body class="mx-auto max-w-xl px-6 mt-2 bg-gradient-to-r from-slate-400 via-gray-300 to-gray-400" >
-    <x-laradumps::success/>
-    <div class="p-3 bg-slate-50 mb-3 rounded-md shadow-lg text-slate-700 border border-slate-400">
-      <h1 class="text-lg">LaraDumps Config for {{ config('app.name') }}</h1>
+  <body class="mx-auto max-w-xl px-6 mt-2 bg-gradient-to-r from-slate-400 via-gray-300 to-gray-400">
+    <x-laradumps::success />
+    <div x-data="{ openTab: 'general' }" class="p-6">
+      <ul class="hidden text-sm font-medium text-center text-gray-500 rounded-lg divide-x divide-gray-200 shadow sm:flex dark:divide-gray-700 dark:text-gray-400 mb-3">
+        <li class="w-full">
+          <a @click="openTab = 'general'" x-bind:class="{ 'bg-gray-200': openTab === 'general' }" href="#" class="inline-block p-4 w-full bg-white hover:text-gray-700 bg-white text-gray-900  rounded-l-lg   active focus:outline-none dark:bg-gray-700 dark:text-white dark:bg-gray-800 dark:hover:bg-gray-700" aria-current="page">General</a>
+        </li>
+        <li class="w-full">
+          <a @click="openTab = 'livewire'" x-bind:class="{ 'bg-gray-200': openTab === 'livewire' }" href="#" class="inline-block p-4 w-full bg-white hover:text-gray-700 hover:bg-gray-50   focus:outline-none dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700">Livewire</a>
+        </li>
+        <li class="w-full">
+          <a @click="openTab = 'export'" x-bind:class="{ 'bg-gray-200': openTab === 'export' }" href="#" class="inline-block p-4 w-full bg-white rounded-r-lg hover:text-gray-700 hover:bg-gray-50  focus:outline-none  dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700">Export</a>
+        </li>
+      </ul>
+      <div class="p-3 bg-slate-50 mb-3 rounded-md shadow-lg text-slate-700 border border-slate-400">
+        <h1 class="text-lg">LaraDumps Config for {{ config('app.name') }}</h1>
+        <form name="form" id="form" method="POST" action="{{ route('laradumps.store') }}"> 
+          @csrf 
+          <div x-show="openTab === 'general'">
+            <div class="p-2 text-slate-500 text-left mt-6 mb-4 text-sm">
+              <p>In this section you can configure and adjust LaraDumps settings.</p>
+              <p>All configuration is stored in <a href="{{ $configFile }}" class="relative leading-7 text-yellow-700 cursor-pointer" rel="nofollow" target="_blank">config/laradumps.php</a>.</p>
+            </div>
+            <hr>
+              <x-laradumps::settings :configKeys=$configKeys tab='general'/>
+          </div>
+          <div x-show="openTab === 'livewire'">
+            <div class="p-2 text-slate-500 text-left mt-6 mb-4 text-sm">
+              <p>In this section you can configure and adjust how LaraDumps debug Livewire Components.</p>
+              <p>All configuration is stored in <a href="{{ $configFile }}" class="relative leading-7 text-yellow-700 cursor-pointer" rel="nofollow" target="_blank">config/laradumps.php</a>.</p>
+            </div>
+            <hr>
+            <x-laradumps::settings :configKeys=$configKeys tab='livewire'/>
+          </div>
+        </form>
+        <div x-show="openTab === 'export'">
+          <div class="p-4 text-slate-500 text-left mt-6 mb-6">
+            <x-laradumps::export />
+          </div>
+        </div>
 
-      <div class="p-4 text-slate-500 text-left mt-6 mb-6">
-        <p>Configure general LaraDumps settings for this project.</p>
-        <br/>
-        <p> All the configuration settings are stored in <a href="{{ $configFile }}" class="relative leading-7 text-yellow-700 cursor-pointer" rel="nofollow" target="_blank">config/laradumps.php</a>. </p>
+          <br/>
+          <hr class="my-2 border-gray-300">
+          <div class="flex flex-wrap items-center md:justify-between justify-center">
+            <div class="w-full px-8 mx-auto text-center text-xs text-gray-400">
+              <p>
+                ⭐ Support LaraDumps, star our <a href="https://github.com/laradumps/laradumps" class="relative leading-7 text-yellow-700 cursor-pointer" rel="nofollow" target="_blank">repository</a>.
+              </p>
+            </div>
+          </div>
+        <!--end of pannel -->
       </div>
-      <hr>
-      <form method="POST" action="{{ route('laradumps.store') }}"> @csrf <div>
-          <div class="px-4 sm:px-3">
-            <ul role="list" class="divide-y divide-slate-200"> @foreach ($configKeys as $configKey) 
-              <li class="flex items-center justify-between py-4">
-                <div class="flex flex-col">
-                  <p class="text-base font-medium text-slate-900" id="privacy-option-1-label">{{ $configKey['title'] }}
-                    <a href="{{ $configKey['doc_link'] }}" class="text-xs relative leading-7 text-yellow-700 cursor-pointer" rel="nofollow" target="_blank">[↗]</a>
-                  </p>
-                  <p class="text-sm text-slate-500" id="privacy-option-1-description">{{ $configKey['description'] }}</p>
-                </div>
-
-                  @if($configKey['type'] == 'text') 
-                    <input type="text" 
-                        required="required" 
-                          name="{{ $configKey['env_key'] }}" id="{{ $configKey['env_key'] }}" 
-                          value="{{ $configKey['current_value'] }}" 
-                          class="appearance-none block p-2 mb-6 w-auto text-sm text-slate-900 bg-slate-50 rounded-lg border border-slate-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-slate-700 dark:border-slate-600 dark:placeholder-slate-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" /> 
-                    @endif
-
-                    @if($configKey['type'] == 'select') 
-                      <select name="{{ $configKey['env_key'] }}" 
-                              id="{{ $configKey['env_key'] }}"
-                              class="appearance-none block p-2 mb-6 w-auto text-sm text-slate-900 bg-slate-50 rounded-lg border border-slate-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-slate-700 dark:border-slate-600 dark:placeholder-slate-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                          @foreach ($configKey['options'] as $option => $label)
-                              <option value="{{ $option }}" {{ $option  ==  $configKey['current_value'] ? 'selected' : '' }}> {{ $label }} </option>
-                          @endforeach
-                      </select>
-                    @endif
-
-                    @if($configKey['type'] == 'toggle')
-                      <x-laradumps::switch :configKey=$configKey />
-                    @endif
-              </li> 
-              @endforeach 
-            </ul>
-          </div>
-          <hr>
-          
-          <div class="text-right mt-3">
-            <button type="submit" class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"> Save & Apply </button>
-          </div>
-        </div>
-      </form>
-      <x-laradumps::export/>
-      <div class="text-left text-sm ml-3 mt-5">
-          <p> ⭐ Support LaraDumps, star our <a href="https://github.com/laradumps/laradumps" class="relative leading-7 text-yellow-700 cursor-pointer" rel="nofollow" target="_blank">GitHub repository</a>! </p>
-        </div>
     </div>
     <script src="//unpkg.com/alpinejs" defer></script>
   </body>
