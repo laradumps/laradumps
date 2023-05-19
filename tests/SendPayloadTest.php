df8066e9ec9a4feebb74c6ecf096d01d9be947a6
Michael Rubél <michael@laravel.software>
2023-05-19 11:29:01 +0200

    Set max `50` ms 🔧

--- tests/SendPayloadTest.php
+++ tests/SendPayloadTest.php
@@ -6,7 +6,7 @@ use LaraDumps\LaraDumps\Support\Dumper;
 use PHPUnit\Framework\Constraint\LessThan;
 
 test('curl resets fast when app is not available', function () {
-    $maxExecutionTime = 30;
+    $maxExecutionTime = 50;
 
     $startTime = now();
 
