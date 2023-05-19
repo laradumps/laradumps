d46a13fb9e2517720d9b581a6dfe55a083153f7b
Michael Rubél <michael@laravel.software>
2023-05-19 10:08:18 +0200

    Fix slow loading when app is off. 🔧

--- src/Actions/SendPayload.php
+++ src/Actions/SendPayload.php
@@ -15,12 +15,13 @@ final class SendPayload
         $curlRequest = curl_init();
 
         curl_setopt_array($curlRequest, [
-            CURLOPT_POST           => true,
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_HTTPHEADER     => ['Content-Type: application/json', 'Accept: application/json'],
-            CURLOPT_POSTFIELDS     => json_encode($payload),
-            CURLOPT_URL            => $appUrl,
+            CURLOPT_POST              => true,
+            CURLOPT_RETURNTRANSFER    => true,
+            CURLOPT_FOLLOWLOCATION    => true,
+            CURLOPT_HTTPHEADER        => ['Content-Type: application/json', 'Accept: application/json'],
+            CURLOPT_POSTFIELDS        => json_encode($payload),
+            CURLOPT_URL               => $appUrl,
+            CURLOPT_CONNECTTIMEOUT_MS => 10,
         ]);
 
         curl_close($curlRequest);
