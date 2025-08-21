fd1d2989b4d7e7ae52516911b638524f81d4a9c8
luanfreitasdev <luanfreitas10@protonmail.com>
2025-08-21 15:32:04 -0300

    Add docs for ignored SQL and route patterns configuration

--- resources/config/laradumps.php
+++ resources/config/laradumps.php
@@ -1,6 +1,15 @@
 <?php
 
 return [
+    /*
+    |--------------------------------------------------------------------------
+    | Ignored Queries Patterns
+    |--------------------------------------------------------------------------
+    |
+    | Define patterns for SQL queries and routes that LaraDumps should ignore.
+    | Useful for preventing logs of repetitive or unnecessary queries/routes.
+    |
+    */
     'queries' => [
         'ignore_sql_patterns' => [
             '', // select * from `*` where `id` = 1
