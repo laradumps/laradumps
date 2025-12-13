4242844c147e5cf95f56fb597eaf69aca0dda28f
luanfreitasdev <luanfreitas10@protonmail.com>
2025-12-13 09:30:23 -0300

    chore: update PHP version requirements and adjust dependencies in composer.json

--- src/Actions/AppendLaradumpsYamlToGitignore.php
+++ src/Actions/AppendLaradumpsYamlToGitignore.php
@@ -6,7 +6,6 @@ final class AppendLaradumpsYamlToGitignore
 {
     public static function handle(): bool
     {
-
         if (! file_exists('.gitignore') || str_contains(file_get_contents('.gitignore'), 'laradumps.yaml')) {
             return false;
         }
